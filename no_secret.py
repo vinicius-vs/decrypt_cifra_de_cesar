c9514600be9ea7a8c6a6af20f6b8106a570f1018
vinicius stumpf <61637439+vinicius-vs@users.noreply.github.com>
2020-09-22 21:58:05 -0300

    Update no_secret.py

--- no_secret.py
+++ no_secret.py
@@ -7,13 +7,26 @@ def ask_file():# função para pedir o arquivo para o usuario.
     return text
 
 def analyze(message): #função para analisar se o testo esta descriptografado.
-    dictionary = open("dicionariopt.txt", "r") # abre o dicionario que sera usado para verificação das palavras.
+    dictionary = open("dicionarioptcompleto.txt",
+                      "r")  # abre o dicionario que sera usado para verificação das palavras.
     dictionary_util = dictionary.readlines()
-    message_util = message.split(" " or "." or "?") # separa o texto em palavras unicas
-    for word in dictionary_util:
-        if word == message_util[0]+'\n':
-            return True
+    message_util = message.split(" " or "." or "?")  # separa o texto em palavras unicas
+
+    cont = 0
+
+    for i in range(len(message_util)):
+        for word in dictionary_util:
+            if word == message_util[i] + '\n':
+                cont = cont + 1
+
+
+
+    percentagem = cont*100/len(message_util)
+
+    if percentagem >= 30.0:
+        return True
     dictionary.close()
+
 
 def decrypt(text, rotation ): #função que realiza a troca de letras.
     aplhabet = ['a', 'b','c', 'd', 'e', 'f', 'g', 'h', 'i','j','k','l','m','n','o', 'p','q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
@@ -37,13 +50,33 @@ def decrypt(text, rotation ): #função que realiza a troca de letras.
             text_decr += new_letter
 
     return text_decr
-
-message = ask_file()
-rotation = 1
 while True:
-    read = decrypt(message, rotation)
-    if analyze(read) == True:
-        break
+    esco = input("Escolha uma opção. D - Decrypt, E - Encripyt: ")
+    if esco == D or esco == E:
+         break
     else:
-        rotation = rotation + 1
-print("\n",read,"\n")
+        print("escolha invalida, tente novamente.\n")
+
+if esco == "E":
+    from random import *
+    rotation = randrange(1 , 25)
+    message = ask_file()
+    encrypt = cifra_cesar(message,rotation)
+    print(encrypt,"\n")
+    print(rotation)
+elif esco =="D":
+    message = ask_file()
+    rotation = 1
+    counter = 0
+    while True:
+
+        if counter == 25:
+            print("Não foi possível quebrar")
+
+        else:
+            read = cifra_cesar(message, rotation)
+            if analyze(read) == True:
+                print("\n", read, "\n")
+                break
+            else:
+                rotation = rotation + 1
